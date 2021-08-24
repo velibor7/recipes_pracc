611201deccdecc0e1a8c8b52174efb7e77b0f7a0
Velibor <veliborvasiljevic7@gmail.com>
2021-08-24 21:39:58 +0200

    tags endpoint

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,8 +1,14 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model 
 
+from core import models
+
 #  get_user_model easier when changing in the future
 
+
+def sample_user(email="test@email.com", password="test123"):
+	"""Create a sample user"""
+	return get_user_model().objects.create_user(email, password)
 
 class ModelTests(TestCase):
 
@@ -44,9 +50,11 @@ class ModelTests(TestCase):
 		self.assertTrue(user.is_superuser)
 		self.assertTrue(user.is_staff)
 
+	def test_tag_str(self):
+		"""Test the tag string representation"""
+		tag = models.Tag.objects.create(
+			user=sample_user(),
+			name='Vegan'
+		)
 
-
-
-
-
-
+		self.assertEqual(str(tag), tag.name)
