7340340c5f6728036437c964de832848a0d8d877
Velibor <veliborvasiljevic7@gmail.com>
2021-08-24 22:01:12 +0200

    create tag endpoint

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -7,7 +7,9 @@ from core.models import Tag
 from recipe import serializers
 
 
-class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+class TagViewSet(viewsets.GenericViewSet, 
+				 mixins.ListModelMixin,
+				 mixins.CreateModelMixin):
 	"""Manage tags in the database"""
 	authentication_classes =  (TokenAuthentication,)
 	permission_classes = (IsAuthenticated,) # user is auth to use api
@@ -16,6 +18,12 @@ class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
 
 	def get_queryset(self):
 		"""Return for the current user"""
-		return self.queryset.filter(user=self.request.user).order_by('-name')
+		return self.queryset.filter(
+			user=self.request.user
+		).order_by('-name')
 
+	def perform_create(self, serializer):
+		"""Create a new tag"""
+		serializer.save(user=self.request.user)
+		
 
