d06198210121419842429398816bb4b8615974dc
Velibor <veliborvasiljevic7@gmail.com>
2021-08-24 22:31:11 +0200

    list ingredient endpoint

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -58,3 +58,12 @@ class ModelTests(TestCase):
 		)
 
 		self.assertEqual(str(tag), tag.name)
+
+	def test_ingredient_str(self):
+		"""Test the ingredient string representation"""
+		ingredient = models.Ingredient.objects.create(
+			user=sample_user(),
+			name='Cucamber'
+		)
+
+		self.assertEqual(str(ingredient), ingredient.name)
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Tag
+from core.models import Tag, Ingredient
 
 from recipe import serializers
 
@@ -11,6 +11,7 @@ class TagViewSet(viewsets.GenericViewSet,
 				 mixins.ListModelMixin,
 				 mixins.CreateModelMixin):
 	"""Manage tags in the database"""
+
 	authentication_classes =  (TokenAuthentication,)
 	permission_classes = (IsAuthenticated,) # user is auth to use api
 	queryset = Tag.objects.all()
@@ -26,4 +27,21 @@ class TagViewSet(viewsets.GenericViewSet,
 		"""Create a new tag"""
 		serializer.save(user=self.request.user)
 		
+
+
+
+class IngredientViewSet(viewsets.GenericViewSet, 
+				 		mixins.ListModelMixin):
+	"""Manage ingredients in the database"""
+
+	authentication_classes =  (TokenAuthentication,)
+	permission_classes = (IsAuthenticated,) # user is auth to use api
+	queryset = Ingredient.objects.all()
+	serializer_class = serializers.IngredientSerializer
+
+	def get_queryset(self):
+		"""Return for the current user"""
+		return self.queryset.filter(
+			user=self.request.user
+		).order_by('-name')
 
